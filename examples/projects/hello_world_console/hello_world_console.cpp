39dc988be8f7e596f1d1c46a94bd98423219f19e
Josh Heitzman <joshheitzman@hotmail.com>
2012-11-27 23:57:18 -0800

    Add hello_world_console example project.
    The hello_world_console project is the minimal Android faux console
    project that is compatible with being executed by the batch file:
    
    %grail42_core_cmd%\android\faux_console\run_app.cmd

---
+++ examples/projects/hello_world_console/hello_world_console.cpp
@@ -0,0 +1,40 @@
+/*                   Copyright (C) 2012 Josh Heitzman
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or online copies at:
+* http://www.boost.org/LICENSE_1_0.txt
+* http://opensource.org/licenses/BSL-1.0
+* http://directory.fsf.org/wiki/License:Boost1.0
+* http://en.wikipedia.org/wiki/Boost_Software_License                */
+
+#include <stdio.h>
+#include <jni.h>
+#include <android/log.h>
+
+int faux_main()
+{
+	printf("Hello world.\n");
+	return 1;
+}
+
+extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM * vm, void * reserved)
+{
+	// Flush the emmission of "In mgmain JNI_OnLoad" by another component prior to
+	// entering the faux main function, so it will be emitted prior to the start 
+	// marker emitted by the activity.
+	fflush(stdout);
+
+	// Returning JNI_VERSION_1_1 on Android 2.2 results in "JNI_OnLoad returned bad version (65537)".
+    return JNI_VERSION_1_2;
+}
+
+extern "C" JNIEXPORT jint JNICALL Java_net_examples_hello_1world_1console_FauxConsole_main(JNIEnv * env, jobject  obj)
+{
+	int result = faux_main();
+
+	// Stdout isn't automatically flushed, so explicitly flush it to ensure all output is logged prior to shutdown.
+	// Also the stdout buffer between the JVM and native code doesn't appear to be shared as flushing from the 
+	// activity has no apparent effect.
+	fflush(stdout);
+
+	return result;
+}
