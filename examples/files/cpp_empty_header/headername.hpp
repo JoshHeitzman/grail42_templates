e89da442e1112d2d2114bdc9b8fc8ee962eb26b8
Josh Heitzman <joshheitzman@hotmail.com>
2012-12-09 14:16:54 -0800

    Add cpp_empty_header template for cloning new C++ headers from

---
+++ examples/files/cpp_empty_header/headername.hpp
@@ -0,0 +1,16 @@
+/*                   Copyright (C) 2012 Josh Heitzman
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or online copies at:
+* http://www.boost.org/LICENSE_1_0.txt
+* http://opensource.org/licenses/BSL-1.0
+* http://directory.fsf.org/wiki/License:Boost1.0
+* http://en.wikipedia.org/wiki/Boost_Software_License                */
+
+#ifndef MACRO_PREFIX_HG_DD5AA59CEB5E44BCB659C8A0CB6138A4
+#define MACRO_PREFIX_HG_DD5AA59CEB5E44BCB659C8A0CB6138A4
+#pragma warning(push)
+#pragma warning(disable : 4068)
+#pragma once
+#pragma warning(pop)
+
+#endif // MACRO_PREFIX_HG_DD5AA59CEB5E44BCB659C8A0CB6138A4
